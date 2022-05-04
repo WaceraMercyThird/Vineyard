334e4bf859e133adb8931dc861d04e7756f9cdd1
Mercy Wacera <waceraaamercy@gmail.com>
2022-05-04 15:56:57 +0300

    add files created

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+R  = float(input("Enter the length of the row in feet: "))
+E = float(input("Enter the amount of space used by end-post assembly in feet: "))
+S = float(input("Enter the amount of space by vines in the row: "))
+
+V = (R - 2 * E) / S
+
+print("The number of grapyings in the row will be:", V)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
